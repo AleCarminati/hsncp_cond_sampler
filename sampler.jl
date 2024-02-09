6ddc2c7a06548c040f6357bbb53c6f6588a26fda
AleCarminati <alessandro4.carminati@mail.polimi.it>
2024-02-09 17:08:31 +0100

    Added functions to allocate/deallocate atoms

--- sampler.jl
+++ sampler.jl
@@ -26,6 +26,23 @@ struct AtomsContainer
   end
 end
 
+function deleteat!(atoms::AtomsContainer; index = h)
+  jump = deleteat!(atoms.jumps, h)
+  location = deleteat!(atoms.locations, h)
+  counter = deleteat!(atoms.counter, h)
+end
+
+function push!(
+  atoms::AtomsContainer;
+  jump = jump,
+  location = location,
+  counter = counter,
+)
+  push!(atoms.jumps, jump)
+  push!(atoms.locations, location)
+  push!(atoms.counter, counter)
+end
+
 struct MCMCState
   # A vector containing, for each group, the auxiliary variable called u_l.
   auxu::Vector{Real}
@@ -63,6 +80,79 @@ end
 
 function initalizemcmcstate!(state::MCMCState)
   # TODO: initalization of the state of the MCMC.
+end
+
+function deallocatechildrenatom!(state::MCMCState; group = l, index = h)
+  #= This function removes the h-indexed allocated atom of the children process
+    of group l from the list of allocated atoms. It also modifies the other
+    elements of the state to maintain coherency. =#
+
+  # Remove all the information of the selected atom from the list of allocated
+  # atoms.
+  jump, location, _ = deleteat!(state.childrenallocatedatoms[l], h)
+
+  # Add the selected atom to the list of non allocated atoms.
+  push!(state.childrennonallocatedatoms[l], jump, location, 0)
+
+  # Remove the clustering label for the selected atom. Indeed, the state does
+  # not contain clustering labels for non allocated atoms.
+  deleteat!(state.childrenatomslabels[l], h)
+
+  #= Given that an allocated atom has been removed, the other allocated atoms
+    with index greater than h had their index reduced by 1. Thus, we reduce the
+    corresponding index in the vector of the within-group clustering labels. =#
+  state.wgroupcluslabels[l] =
+    state.wgroupcluslabels[l] - (state.wgroupcluslabels[l] .> h)
+end
+
+function allocatechildrenatom!(state::MCMCState; group = l, index = h)
+  #= This function adds the h-indexed non allocated atom of the children process
+    of group l to the list of allocated atoms. It also modifies the other
+    elements of the state to maintain coherency.
+    This function does not change the clustering labels, it just initializes the
+    counter of the selected atom to 1. =#
+
+  # Remove all the information of the selected atom from the list of non
+  # allocated atoms.
+  jump, location, _ = deleteat!(state.childrennonallocatedatoms[l], h)
+
+  # Add the selected atom to the list of non allocated atoms.
+  push!(state.childrenallocatedatoms[l], jump, location, 1)
+end
+
+function deallocatemotheratom!(state::MCMCState; index = j)
+  #= This function removes the j-indexed allocated atom of the mother process
+    from the list of allocated atoms. It also modifies the other
+    elements of the state to maintain coherency. =#
+
+  # Remove all the information of the selected atom from the list of allocated
+  # atoms.
+  jump, location, _ = deleteat!(state.motherallocatedatoms, j)
+
+  # Add the selected atom to the list of non allocated atoms.
+  push!(state.mothernonallocatedatoms, jump, location, 0)
+
+  #= Given that an allocated atom has been removed, the other allocated atoms
+    with index greater than j had their index reduced by 1. Thus, we reduce the
+    corresponding index in the vector of the children atoms clustering labels.
+    We use the map() function because this operation must be repeated for every
+    group. =#
+  state.childrenatomslabels = map(x -> x .- (x .> j), state.childrenatomslabels)
+end
+
+function allocatemotheratom!(state::MCMCState; index = j)
+  #= This function adds the j-indexed non allocated atom of the mother process
+    to the list of allocated atoms. It also modifies the other elements of the
+    state to maintain coherency.
+    This function does not change the clustering labels, it just initializes the
+    counter of the selected atom to 1. =#
+
+  # Remove all the information of the selected atom from the list of non
+  # allocated atoms.
+  jump, location, _ = deleteat!(state.mothernonallocatedatoms, j)
+
+  # Add the selected atom to the list of non allocated atoms.
+  push!(state.motherallocatedatoms, jump, location, 1)
 end
 
 struct MCMCOutput
