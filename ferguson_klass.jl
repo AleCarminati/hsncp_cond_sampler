13c2763da43451012e1763f4d4ae6bacf0d9a5d6
AleCarminati <alessandro4.carminati@mail.polimi.it>
2024-02-22 02:44:17 +0100

    Improve research of the first jump

--- ferguson_klass.jl
+++ ferguson_klass.jl
@@ -4,8 +4,7 @@
 function fergusonklass(func, epsilon)
   # func must be a univariate function that returns the integral of the jump
   # part of the Lévy intensity from x to +infinity, where x is func's input.
-
-  output = []
+  output = Real[]
   expdist = Exponential(1)
 
   # The value of the last sample of the auxiliary variable.
@@ -24,7 +23,29 @@ function fergusonklass(func, epsilon)
         Roots.Bisection(),
       )
     else
-      jump = Roots.find_zero(x -> func(x) - auxvar, 0)
+      # Search for a limit of the interval where to search the value of the
+      # first jump.
+      limitabove = 1
+      if func(0) - auxvar > 0
+        while func(limitabove) - auxvar > 0
+          limitabove += 0.25
+          if limitabove > 1000
+            error("Failed to find the value of the first jump.")
+          end
+        end
+      else
+        while func(limitabove) - auxvar < 0
+          limitabove += 0.25
+          if limitabove > 1000
+            error("Failed to find the value of the first jump.")
+          end
+        end
+      end
+      jump = Roots.find_zero(
+        x -> func(x) - auxvar,
+        (0, limitabove),
+        Roots.Bisection(),
+      )
     end
 
     push!(output, jump)
