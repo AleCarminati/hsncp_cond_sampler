a78f1675eb82e07cea912b7fd0c01b223cd7739b
AleCarminati <alessandro4.carminati@mail.polimi.it>
2024-02-28 14:40:12 +0100

    Points divided in groups in cluster plot

--- run.jl
+++ run.jl
@@ -5,12 +5,13 @@ seed = 23478747247
 Random.seed!(seed)
 
 g = 3
+n = [50, 50, 50]
 components = Normal.([-5, 0, 5], [1, 1, 1])
-trueclust = [rand(1:3, 50) for l = 1:g]
+trueclust = [rand(1:3, n[l]) for l = 1:g]
 data = [rand.(components[trueclust[l]]) for l = 1:g]
 input = MCMCInput(data)
 
-model = NormalMeanModel(1, 1, 0.5, 1, 1)
+model = NormalMeanModel(1, 1, 1, 1, 1)
 
 output = hsncpmixturemodel_fit(
   input,
@@ -30,14 +31,23 @@ R"bestclust = salso(clus)"
 
 @rget bestclust
 
+ticksvalues = vcat(0:0.5:((g-1)*0.5), ((g+1)*0.5):0.5:g)
+tickslabels =
+  vcat("Estimated - group " .* string.(1:g), "True - group " .* string.(1:g))
+
+yvalues = vcat([fill((l - 1) * 0.5, n[l]) for l = 1:g]...)
+yvalues =
+  vcat(yvalues, [fill(((g + 1) * 0.5) + (l - 1) * 0.5, n[l]) for l = 1:g]...)
+
 savefig(
   scatter(
     repeat(vcat(data...), 2),
-    vcat(fill(1, 150), fill(2, 150)),
+    yvalues,
     color = vcat(bestclust, vcat(trueclust...)),
-    yticks = ([1, 2], ["Estimated clustering", "True clustering"]),
+    yticks = (ticksvalues, tickslabels),
     legend = false,
     size = (1500, 700),
+    title = "Across-group clustering",
   ),
   "clust.png",
 )
