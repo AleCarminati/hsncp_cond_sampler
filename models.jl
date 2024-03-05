17b856cbdcee06908ef3094f97b7af7ff918daef
AleCarminati <alessandro4.carminati@mail.polimi.it>
2024-03-05 13:49:11 +0100

    Added NormalMeanVarModel and sampling functions

--- models.jl
+++ models.jl
@@ -3,7 +3,9 @@
 
 abstract type Model end
 
-struct NormalMeanModel <: Model
+abstract type GammaCRMModel <: Model end
+
+@kwdef struct NormalMeanModel <: GammaCRMModel
   #=
   		- Gaussian mixture components:
   			- fixed variance;
@@ -26,4 +28,64 @@ struct NormalMeanModel <: Model
   # Hyperparameters for the Gamma CRM of the mother process.
   mothertotalmass::Real
   motherlocsd::Real
+end
+
+function samplepriormotherloc(model::NormalMeanModel, n)
+  # Function that samples n locations from the prior of the mother process.
+  return rand.(fill(Normal(0, model.motherlocsd), n), 1)
+end
+
+function samplechildloc(model::NormalMeanModel, associatedmotheratomloc)
+  # Function that samples the location of the children atoms based on its
+  # associated mother atoms.
+  return map(
+    x -> rand(Normal(x[1], model.kernelsd), 1),
+    associatedmotheratomloc,
+  )
+end
+
+@kwdef struct NormalMeanVarModel <: GammaCRMModel
+  #=
+      - Gaussian mixture components:
+        - fixed variance;
+        - random mean.
+      - Child processes: Gamma CRM with fixed total mass, for the locations we
+        use a Gaussian kernel with variance given by the second element of the
+        mother process atom's location.
+      - Mother process: Gamma CRM with fixed total mass, for the locations we
+        use a Gaussian with fixed variance and an inverse gamma with fixed shape
+        and scale.
+    =#
+
+  # Standard deviation of the Gaussian mixture components.
+  mixturecompsd::Real
+
+  # Total mass for the Gamma CRM of the child processes.
+  childrentotalmass::Real
+
+  # Hyperparameters for the Gamma CRM of the mother process.
+  mothertotalmass::Real
+  motherlocsd::Real
+  motherlocshape::Real
+  motherlocscale::Real
+end
+
+function samplepriormotherloc(model::NormalMeanVarModel, n)
+  # Function that samples n locations from the prior of the mother process.
+  return map(
+    x -> rand.(x),
+    fill(
+      [
+        Normal(0, model.motherlocsd),
+        InverseGamma(model.motherlocshape, model.motherlocscale),
+      ],
+      n,
+    ),
+  )
+end
+
+function samplechildloc(model::NormalMeanVarModel, associatedmotheratomloc)
+  # Function that samples the location of the children atoms based on its
+  # associated mother atoms.
+  return map(x -> rand(Normal(x[1], x[2]), 1), associatedmotheratomloc)
 end
